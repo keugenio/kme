f484c0c7b1f8d8c583b59fba3f702a03b36d10d8
Unknown <kui_eugenio@hotmail.com>
2019-10-07 08:46:36 -0700

    init website files

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <Kumbotron />
+    <AboutMeCards />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Kumbotron from '@/components/Kumbotron.vue'
+import Footer from '@/components/Footer.vue'
+import AboutMeCards from '@/components/AboutMeCards.vue'
+export default {
+  name: "home",
+  components: {
+    Kumbotron, Footer, AboutMeCards
+  }
+};
+</script>
